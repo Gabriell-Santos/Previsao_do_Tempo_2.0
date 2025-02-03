c67b81c4da05248f15a63535c9b757b9d2435d6d
Gabriel <ferreirag.santos.dev@gmail.com>
2025-02-03 10:17:55 -0300

    tratando todoos os dados retornados da api

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,15 @@ function App() {
     /* Criando a Função de Busca */
   }
   function searchCity() {
-    console.log(InputRef.current.value);
+    const Api = "62b7a6d0102fcb62feeda09e8b75a278";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api}&units=metric&lang=pt_br`
+    const city = InputRef.current.value
   }
 
   return (
     <div>
       <h2>Previsão do Tempo 2.0</h2>
-      <input  ref={InputRef} type="text" placeholder="Digite o nome da Cidade" />
+      <input ref={InputRef} type="text" placeholder="Digite o nome da Cidade" />
       <button onClick={searchCity}>Buscar</button>
     </div>
   );
