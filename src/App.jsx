32a2359e6ff694d710da8958fc86e392d1684428
Gabriel <ferreirag.santos.dev@gmail.com>
2025-02-03 10:27:10 -0300

    instalando o axios, para acessar o servidor

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
@@ -8,10 +9,12 @@ function App() {
   {
     /* Criando a Função de Busca */
   }
-  function searchCity() {
+  async function searchCity() {
+    const city = InputRef.current.value;
     const Api = "62b7a6d0102fcb62feeda09e8b75a278";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api}&units=metric&lang=pt_br`
-    const city = InputRef.current.value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api}&units=metric&lang=pt_br`;
+    const data = await axios.get(url);
+    console.log(data)
   }
 
   return (
