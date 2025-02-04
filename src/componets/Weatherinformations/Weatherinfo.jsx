b029b918f705ebd383176e75451cc8ffd7e60f4c
Gabriel <ferreirag.santos.dev@gmail.com>
2025-02-04 11:11:48 -0300

    colocando as informações na tela

--- src/componets/Weatherinformations/Weatherinfo.jsx
+++ src/componets/Weatherinformations/Weatherinfo.jsx
@@ -2,9 +2,23 @@ import "./Weatherinfo.css"
 import React from 'react'
 
 const Weatherinfo = ({info}) => {
+    
+    console.log(info);
+
   return (
     <div>
-        <h3>Previsões do Dia!</h3>
+        <h4>Previsões do Dia!</h4>
+        <h2> {info.name} </h2>
+        <div>
+        <img src={`http://openweathermap.org/img/wn/${info.weather[0].icon}.png`} alt="Icone do Clima" />
+        <p> {Math.round(info.main.temp)}° C </p>
+        </div>
+        <p> {info.weather[0].description} </p>
+        <div>
+            <p>Sensação térmica: </p>
+            <p>Umidade:  </p>
+            <p>Pressão:  </p>
+        </div>
     </div>
   )
 }
