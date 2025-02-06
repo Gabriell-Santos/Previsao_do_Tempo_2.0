d5311bf08c938708186428a8a347fccdd0589a53
Gabriel <ferreirag.santos.dev@gmail.com>
2025-02-06 09:40:10 -0300

    Preparando o componente WeatherInformantion para a estilização

--- src/componets/Weatherinformations/Weatherinfo.jsx
+++ src/componets/Weatherinformations/Weatherinfo.jsx
@@ -6,14 +6,14 @@ const Weatherinfo = ({info}) => {
     console.log(info);
 
   return (
-    <div>
+    <div className="container">
         <h4>Previsões do Dia!</h4>
         <h2> {info.name} </h2>
-        <div>
+        <div className="weather-info">
         <img src={`http://openweathermap.org/img/wn/${info.weather[0].icon}.png`} alt="Icone do Clima" />
-        <p> {Math.round(info.main.temp)}° C </p>
+        <p className="temperature"> {Math.round(info.main.temp)}° C </p>
         </div>
-        <p> {info.weather[0].description} </p>
+        <p className="description"> {info.weather[0].description} </p>
         <div>
             <p>Sensação térmica: {Math.round(info.main.feels_like)} °C </p>
             <p>Umidade: {info.main.humidity} </p>
