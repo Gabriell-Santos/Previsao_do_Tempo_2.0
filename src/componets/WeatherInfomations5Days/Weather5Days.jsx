131f09fce1152e0100eb5db0f372c9965d2825b3
Gabriel <ferreirag.santos.dev@gmail.com>
2025-02-11 15:35:37 -0300

    criando novo componente para as informações da previsão de 5 dias

---
+++ src/componets/WeatherInfomations5Days/Weather5Days.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Weather5Days"
+
+const Weather5Days = () => {
+  return (
+    <div>
+        <h2>Previsão daqui 5 Dias </h2>
+    </div>
+  )
+}
+
+export default Weather5Days
